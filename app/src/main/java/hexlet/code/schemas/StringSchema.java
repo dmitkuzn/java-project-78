f64cd9c886251be0d9d6e54801cc4a3097edb12c
Dmitrii Kuznetsov <dmitkuzn@mail.ru>
2024-11-24 21:45:09 +0300

    123

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -2,19 +2,43 @@ package hexlet.code.schemas;
 
 public class StringSchema {
 
+    private boolean isRequired;
+    private int minLenght;
+    private String strContains;
+
     public boolean isValid(String str) {
-        return false;
+        System.out.println("isValid");
+        System.out.println(str);
+        if (isRequired && (str == null || str.length() == 0)) {
+            return false;
+        }
+        if ((minLenght != -1) && (str.length() < minLenght)) {
+            return false;
+        }
+        if ((strContains != null) && (str.indexOf(strContains) < 0)) {
+            return false;
+        }
+        return true;
+    }
+
+    public StringSchema() {
+        isRequired = false;
+        minLenght = -1;
+        strContains = null;
     }
 
     public StringSchema required() {
+        isRequired = true;
         return this;
     }
 
     public StringSchema minLength(int min) {
+        minLenght = min;
         return this;
     }
 
     public StringSchema contains(String str) {
+        strContains = str;
         return this;
     }
 
--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -13,20 +13,21 @@ public class AppTest {
         var v = new Validator();
         var schema = v.string();
 
-        assertTrue(schema.isValid(null));
+        assertTrue(schema.isValid(null), "Expected valid for null");
         schema.required();
-        assertFalse(schema.isValid(null));
-        assertFalse(schema.isValid(""));
+        assertFalse(schema.isValid(null), "Expected not valid for null");
+        assertFalse(schema.isValid(""), "Expected not valid for empty");
 
-        assertTrue(schema.isValid("what does the fox say"));
-        assertTrue(schema.isValid("hexlet"));
+        assertTrue(schema.isValid("what does the fox say"), "Expected valid for string");
+        assertTrue(schema.isValid("hexlet"), "Expected valid for string");
 
-        assertTrue(schema.contains("wh").isValid("what does the fox say"));
-        assertTrue(schema.contains("what").isValid("what does the fox say"));
-        assertFalse(schema.contains("whatthe").isValid("what does the fox say"));
+        assertTrue(schema.contains("wh").isValid("what does the fox say"), "Expected valid for string contains wf");
+        assertTrue(schema.contains("what").isValid("what does the fox say"), "Expected valid for string contains what");
+        assertFalse(schema.contains("whatthe").isValid("what does the fox say"), "Exp notvalid for contains whatthe");
 
         var schema1 = v.string();
-        assertEquals(schema1.minLength(10).minLength(4).isValid("Hexlet"), false);
+        assertEquals(false, schema1.minLength(10).isValid("Hexlet"), "Expected notvalid for minlen 10");
+        assertEquals(true, schema1.minLength(10).minLength(4).isValid("Hexlet"), "Expected valid for string minlen");
     }
 
 }
